c4b7f2d6a14d52819cb514e1264f59c243b8e4f3
Jase Cutler <jasecutler@gmail.com>
2017-07-24 16:10:12 -0700

    Clean up and comments

--- app/index.js
+++ app/index.js
@@ -4,12 +4,14 @@ moment().format();
 
 let now = moment()
 
+//Output to the console
 console.log("It is " + chalk.blue(now.format("dddd, MMMM Do YYYY, h:mm:ss a")) + ".")
 console.log("It is the " + chalk.magentaBright(now.dayOfYear()) + "th day of the year.")
 console.log("It is "+ chalk.greenBright(findSeconds(now)) +" seconds into the day.")
 console.log("It " + findDaylight(now) + " during Daylight Savings Time.")
 console.log("It " + findLeapYear(now) + " a leap year.")
 
+//This calculates the seconds in the day
 function findSeconds(now) {
     let seconds = 0
     seconds += +now.format("H") * 60 * 60
@@ -17,7 +19,7 @@ function findSeconds(now) {
     seconds += +now.format("s")
     return seconds
 }
-
+//Returns the formatted text according to true false
 function findDaylight(now) {
     if(now.isDST()){
         return chalk.green("is")
